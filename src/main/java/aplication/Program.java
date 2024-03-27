32050dee3715b170864533afbd5e2548fe23988a
Weslley Coelho <wes.coelho18@gmail.com>
2024-03-26 23:01:42 -0300

    implementação do método de processamento invoice

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -2,6 +2,8 @@ package aplication;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -12,6 +14,8 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         System.out.println("Entre com os dados do aluguel:");
@@ -19,15 +23,28 @@ public class Program {
 
         Vehicle carModel = new Vehicle(sc.nextLine());
 
-        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
         LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
 
-        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+        System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
-
 
         CarRental carRental = new CarRental(start, finish, carModel);
 
+        System.out.println("Entre com o preço por hora:");
+        double pricePerHour = sc.nextDouble();
+
+        System.out.println("Entre com o preço por dia:");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService =  new RentalService(pricePerDay,pricePerHour, new BrazilTaxService());
+
+        rentalService.processInvoice(carRental);
+
+        System.out.println("FATURA: ");
+        System.out.println("Pagamento básico: " + String.format("%.2f", carRental.getInvoice().getBasicPayment()));
+        System.out.println("Imposto: " + String.format("%.2f", carRental.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + carRental.getInvoice().getTotalPayment());
 
         sc.close();
 
